a7d6e2f112bdf8300cda086f82227e5d48744773
James Albright <G00379271@atu.ie>
2023-10-04 17:48:41 +0100

    defined attributes of book class

--- src/main/java/ie/atu/week3_library/Book.java
+++ src/main/java/ie/atu/week3_library/Book.java
@@ -1,5 +1,19 @@
 package ie.atu.week3_library;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Book {
+
+    private String title;
+    private String author;
+    private int isbn;
+    private int publishedYear;
 
 }
