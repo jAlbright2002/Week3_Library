2e54fce27eb5bc960cdec767774164fc730fd9bf
James Albright <G00379271@atu.ie>
2023-10-04 17:53:01 +0100

    created LibraryService class that adds and gets books from Book class

---
+++ src/main/java/ie/atu/week3_library/LibraryService.java
@@ -0,0 +1,20 @@
+package ie.atu.week3_library;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryService {
+
+
+    public void addBook() {
+        //code to add books to the library
+    }
+
+    public void getBook() {
+        //code to retrieve a list of all books in library
+    }
+
+
+}
+
+
